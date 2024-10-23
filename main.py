1bfb6058daae91d6eee726919a6b3ba498baff62
hunman89 <hunman89@gmail.com>
2024-10-23 21:36:54 +0900

    feat. 데이터 임베딩 - openai text-embedding-3-small

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pandas as pd
+from openai import OpenAI
+from config import OPENAI_API_KEY
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 data = pd.read_pickle('./final_result.pkl')
 df = pd.DataFrame(list(data.items()), columns=['question', 'answer'])
-print(df.head())
+
+def get_embedding(text, model="text-embedding-3-small"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+df['question_embedding'] = df['question'].apply(get_embedding)
+df.to_csv('embedded.csv')
